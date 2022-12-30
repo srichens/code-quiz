1f11055a1d5e807f7245e8693ccc16c76a4e5cd6
Sarah Richens <sarahgrichens@gmail.com>
2022-12-30 10:55:19 -0600

    add start quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,33 @@
-var count = 0;
-var startEl = document.querySelector("#start-button");
-var countEl = document.querySelector("#timer-count");
+let count = 60;
+let startEl = document.querySelector("#start-button");
+let countEl = document.querySelector("#timer-count");
+let questionEl = document.querySelector("#question-title");
 
 function countDown() {
     count = 60; 
     let myInterval = setInterval(counting,1000); 
     function counting(){
         count--; 
-        countEl.textContent = count; 
+        countEl.textContent = count;
+        if (count === 59){
+            startQuiz();} 
         if(count === 0) {
             clearInterval(myInterval);
-        if (document.querySelector(".word-blanks").innerHTML = "Javascript"){clearInterval(myInterval); }
         }
     }
 }
 
 startEl.addEventListener("click", countDown);
+
+/*let question = document.createElement(questionSentence);
+let answerOne = document.createElement(firstOption);
+let answerTwo = document.createElement(seconOption);
+let answerThree = document.createElement(thirdOption);*/
+
+function startQuiz(){
+    console.log("quiz has started");
+    //questionEl.textContent = "";
+    //question.textContent = "Is this the first question?"
+    //document.body.appendChild(question);
+    
+}
