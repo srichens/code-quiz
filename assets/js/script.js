40d234da652c2e4497c2bdb71236eaf84459ae8a
Sarah Richens <sarahgrichens@gmail.com>
2023-01-01 13:37:19 -0600

    clean up and modify html css and js so that three questions each function and are visible at the right time

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,7 @@ let countEl = document.querySelector("#timer-count");
 let questionEl = document.querySelector("#question-title");
 let answersEl = document.querySelector("#answers");
 let answers2El = document.querySelector("#answers2");
-/*let button1El = document.querySelector("#button1");
-let button2El = document.querySelector("#button2");
-let button3El = document.querySelector("#button3");
-let rightWrong = document.querySelector("#right-wrong-message");
-let button1Name = document.querySelector("#button1").name;
-let button2Name = document.querySelector("#button2").name;
-let button3Name = document.querySelector("#button3").name;
-
-console.log(button1El.name);
-console.log(button2El.name);
-console.log(button3El.name);
-console.log(button1Name);*/
+let answers3El = document.querySelector("#answers3");
 
 function countDown() {
     count = 60; 
@@ -33,40 +22,7 @@ function countDown() {
 }
 
 startEl.addEventListener("click", countDown);
-
-    let button1Name = document.querySelector("#button1").name;
-    let button2Name = document.querySelector("#button2").name;
-    let button3Name = document.querySelector("#button3").name;
-
-    console.log(button1Name);
-    console.log(button2Name);
-    console.log(button3Name);       
     
-    
-
-    function message1(){
-        if (button1Name === "correct") {alert("Correct!")} 
-        else {alert("Incorrect!")}; 
-        secondQuestion();
-    }
-
-    function message2(){
-        if (button2Name === "correct") {alert("Correct!")} 
-        else {alert("Incorrect!")}; 
-         secondQuestion();
-    }
-
-    function message3(){
-        if (button3Name === "correct") {alert("Correct!")} 
-        else {alert("Incorrect!")}; 
-        secondQuestion();
-    }   
-
-    /*function nextQuestion() {
-        if ( questionEl.textContent = "Is this the first question?") {secondQuestion}
-        else if (questionEl.textContent = "Is this the second question?") {thirdQuestion};
-    }*/
-
 function startQuiz(){
     console.log("quiz has started");
     questionEl.textContent = "Is this the first question?";
@@ -75,33 +31,27 @@ function startQuiz(){
     let button1El = document.querySelector("#button1");
     let button2El = document.querySelector("#button2");
     let button3El = document.querySelector("#button3");
-
-    button1El.addEventListener("click", message1);
-    button2El.addEventListener("click", message2);
-    button3El.addEventListener("click", message3);
         
-    
     button1El.textContent = "This is the first answer to the first question";
     button2El.textContent = "This is the second answer to the first question";
     button3El.textContent = "This is the third answer to the first question";
-          
+    
+    button1El.addEventListener("click", message1);
+    button2El.addEventListener("click", message2);
+    button3El.addEventListener("click", message3);
 
     button1El.setAttribute("name", "correct");
     button2El.setAttribute("name", "incorrect");
     button3El.setAttribute("name", "incorrect");
 
-    /*let button1Name = document.querySelector("#button1").name;
+    let button1Name = document.querySelector("#button1").name;
     let button2Name = document.querySelector("#button2").name;
     let button3Name = document.querySelector("#button3").name;
 
     console.log(button1Name);
     console.log(button2Name);
     console.log(button3Name);       
-    
-    button1El.addEventListener("click", message1);
-    button2El.addEventListener("click", message2);
-    button3El.addEventListener("click", message3);
-
+  
     function message1(){
         if (button1Name === "correct") {alert("Correct!")} 
         else {alert("Incorrect!")}; 
@@ -118,7 +68,7 @@ function startQuiz(){
         if (button3Name === "correct") {alert("Correct!")} 
         else {alert("Incorrect!")}; 
         secondQuestion();
-    }*/   
+    }
    
     countEl.textContent = count;
     if(count === 0) {
@@ -129,34 +79,35 @@ function startQuiz(){
 
 function secondQuestion(){
     console.log("this is the second question");
-    questionEl.textContent = "Is this the second question?";
     answersEl.style.visibility = "hidden";
     answers2El.style.visibility = "visible";
+    questionEl.textContent = "Is this the second question?";
 
-    let button21El = document.querySelector("#button1");
-    let button22El = document.querySelector("#button2");
-    let button23El = document.querySelector("#button3");
-    
-    
+    let button21El = document.querySelector("#button21");
+    let button22El = document.querySelector("#button22");
+    let button23El = document.querySelector("#button23");
+        
     button21El.textContent = "This is the first answer to second question";
     button22El.textContent = "This is the second answer to the second question";
     button23El.textContent = "This is the third answer to the second question";
+
+    button21El.addEventListener("click", message21);
+    button22El.addEventListener("click", message22);
+    button23El.addEventListener("click", message23);
 
     button21El.setAttribute("name", "incorrect");
     button22El.setAttribute("name", "correct");
     button23El.setAttribute("name", "incorrect");
 
-    let button21Name = document.querySelector("#button1").name;
-    let button22Name = document.querySelector("#button2").name;
-    let button23Name = document.querySelector("#button3").name;
+    let button21Name = document.querySelector("#button21").name;
+    let button22Name = document.querySelector("#button22").name;
+    let button23Name = document.querySelector("#button23").name;
 
     console.log(button21Name);
     console.log(button22Name);
     console.log(button23Name);  
        
-    button21El.addEventListener("click", message21);
-    button22El.addEventListener("click", message22);
-    button23El.addEventListener("click", message23);
+   
 
     function message21(){
         if (button21Name === "correct") {alert("Correct!")} 
@@ -187,37 +138,63 @@ function secondQuestion(){
 
 function thirdQuestion(){
     console.log("this is the third question");
+    answersEl.style.visibility = "hidden";
+    answers2El.style.visibility = "hidden";
+    answers3El.style.visibility = "visible";
     questionEl.textContent = "Is this the third question?";
-    answersEl.setAttribute("style", "color:black");
-    button1El.setAttribute("style", "color:black; background-color:lightgrey; border:solid;");
-    button2El.setAttribute("style", "color:black; background-color:lightgrey; border:solid;");
-    button3El.setAttribute("style", "color:black; background-color:lightgrey; border:solid;");
 
-    button1El.textContent = "This is the first answer to the third question";
-    button2El.textContent = "This is the second answer to the third question";
-    button3El.textContent = "This is the third answer to the third question";
+    let button31El = document.querySelector("#button31");
+    let button32El = document.querySelector("#button32");
+    let button33El = document.querySelector("#button33");
+    
+    button31El.textContent = "This is the first answer to the third question";
+    button32El.textContent = "This is the second answer to the third question";
+    button33El.textContent = "This is the third answer to the third question";
 
-    button1El.addEventListener("click", correctMessage);
-    button2El.addEventListener("click", incorrectMessage);
-    button3El.addEventListener("click", incorrectMessage);
+    button31El.addEventListener("click", message31);
+    button32El.addEventListener("click", message32);
+    button33El.addEventListener("click", message33);
 
-    function correctMessage(){
-        alert("Correct!"); scorePage();
+    button31El.setAttribute("name", "incorrect");
+    button32El.setAttribute("name", "incorrect");
+    button33El.setAttribute("name", "correct");
+
+    let button31Name = document.querySelector("#button31").name;
+    let button32Name = document.querySelector("#button32").name;
+    let button33Name = document.querySelector("#button33").name;
+
+    console.log(button31Name);
+    console.log(button32Name);
+    console.log(button33Name);  
+       
+    function message31(){
+        if (button31Name === "correct") {alert("Correct!")} 
+        else {alert("Incorrect!")}; 
+        scorePage();
     }
 
-    function incorrectMessage(){
-        alert("Incorrect!"); scorePage();
+    function message32(){
+        if (button32Name === "correct") {alert("Correct!")} 
+        else {alert("Incorrect!")}; 
+        scorePage();
     }
 
+    function message33(){
+        if (button33Name === "correct") {alert("Correct!")} 
+        else {alert("Incorrect!")}; 
+        scorePage();
+    }   
+   
     countEl.textContent = count;
     if(count === 0) {
         clearInterval(myInterval); 
-    }
+    }    
         
     
 }
 
 function scorePage(){
     console.log("this is the score page");
+    answers3El.style.visibility = "hidden";
     questionEl.textContent = "You got __ questions correct";
 }
