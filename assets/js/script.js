7592501e1b0e724118b901f5a910d07c3c14106d
Sarah Richens <sarahgrichens@gmail.com>
2023-01-02 17:21:06 -0600

    add questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,6 @@ let answersEl = document.querySelector("#answers");
 let answers2El = document.querySelector("#answers2");
 let answers3El = document.querySelector("#answers3");
 let divEl = document.querySelector("div");
-//let nameAdd = document.querySelector("#name");
-//let userName = 0;
-//let totalScore = 0;
 let score1 = 0;
 let score2 = 0;
 let score3 = 0;
@@ -35,16 +32,16 @@ startEl.addEventListener("click", countDown);
     
 function startQuiz(){
     console.log("quiz has started");
-    questionEl.textContent = "Is this the first question?";
+    questionEl.textContent = "Who invented Javascript and in what year?";
     answersEl.style.visibility = "visible";
 
     let button1El = document.querySelector("#button1");
     let button2El = document.querySelector("#button2");
     let button3El = document.querySelector("#button3");
         
-    button1El.textContent = "1. This is the first answer to the first question";
-    button2El.textContent = "2. This is the second answer to the first question";
-    button3El.textContent = "3. This is the third answer to the first question";
+    button1El.textContent = "1. Brendan Eich in 1995";
+    button2El.textContent = "2. Steve Jobs in 1984";
+    button3El.textContent = "3. Elon Musk in 2053";
     
     button1El.addEventListener("click", message1);
     button2El.addEventListener("click", message2);
@@ -73,20 +70,20 @@ function startQuiz(){
 }
 
 function secondQuestion(){
-    console.log("this is the second question");
+    console.log("This is the second question");
     answersEl.style.visibility = "hidden";
     answers2El.style.visibility = "visible";
     divEl.style.flexDirection = "row";
     divEl.style.justifyContent = "space-evenly";
-    questionEl.textContent = "Is this the second question?";
+    questionEl.textContent = "What method attaches an event handler to a specified element?";
 
     let button21El = document.querySelector("#button21");
     let button22El = document.querySelector("#button22");
     let button23El = document.querySelector("#button23");
         
-    button21El.textContent = "1. This is the first answer to second question";
-    button22El.textContent = "2. This is the second answer to the second question";
-    button23El.textContent = "3. This is the third answer to the second question";
+    button21El.textContent = "1. toUpperCase()";
+    button22El.textContent = "2. addEventListener()";
+    button23El.textContent = "3. addButtonClick()";
 
     button21El.addEventListener("click", message21);
     button22El.addEventListener("click", message22);
@@ -119,15 +116,15 @@ function thirdQuestion(){
     answers2El.style.visibility = "hidden";
     answers3El.style.visibility = "visible";
     divEl.style.flexDirection = "column-reverse";
-    questionEl.textContent = "Is this the third question?";
+    questionEl.textContent = "What method cancels an event (if it is cancelable)?";
 
     let button31El = document.querySelector("#button31");
     let button32El = document.querySelector("#button32");
     let button33El = document.querySelector("#button33");
     
-    button31El.textContent = "1. This is the first answer to the third question";
-    button32El.textContent = "2. This is the second answer to the third question";
-    button33El.textContent = "3. This is the third answer to the third question";
+    button31El.textContent = "1. Event Bubbling";
+    button32El.textContent = "2. getElementById()";
+    button33El.textContent = "3. preventDefault()";
 
     button31El.addEventListener("click", message31);
     button32El.addEventListener("click", message32);
@@ -187,7 +184,7 @@ function scorePage(){
 
     let submitEl = document.querySelector("#submit");
     let submissionResponseEl = document.querySelector("#submission");
-    let highScoreEl = document.querySelector("#high-score");
+    
 
     let nameAdd = document.querySelector("#name");
        
@@ -196,16 +193,22 @@ function scorePage(){
 
     function showResponse(event) {
         event.preventDefault();
-        let userName = nameAdd.value;
-        let totalScore = score;  
+        console.log(score);
+        console.log(nameAdd.value);
+        let lastUserName = nameAdd.value;
+        let lastTotalScore = score;  
         let response = "Thank you. Your score has been recorded.";
-        let highScoreMessage = "The high score is " + totalScore + " by " + userName;
-        submissionResponseEl.textContent = response;
+        //let lastScore = localStorage.getItem("lastTotalScore");
+       // let lastName = localStorage.getItem("lastUserName");
+        let highScoreEl = document.querySelector("#high-score");
+        let highScoreMessage = "The last score was " + lastTotalScore + " by " + lastUserName;
         highScoreEl.textContent = highScoreMessage;
+       
+        submissionResponseEl.textContent = response;
+        
 
-        localStorage.setItem("userScore", totalScore);
-        localStorage.setItem("userName", userName);
-              
+        localStorage.setItem("lastTotalScore", lastTotalScore);
+        localStorage.setItem("lastUserName", lastUserName);          
                
     }
 }
